71d64253a2c1a79e50a34758883562794ae95ea9
Sofia <cuzyutinasofia@yandex.ru>
2016-10-26 18:15:54 +0300

    iTry - new Project

---
+++ to_do_list/app/src/main/java/com/developers/notes/CreateNoteActivity.java
@@ -0,0 +1,184 @@
+package com.developers.notes;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    public static final String FILE_EXTENSION = "note";
+    EditText noteContentText;
+    EditText noteNameText;
+    Button btnDone;
+    DBHelper dbHelper;
+    SQLiteDatabase notesDB;
+    Intent intent;
+    Calendar call = Calendar.getInstance();
+    int DIALOG_TIME = 1;
+    int myHour = call.get(Calendar.HOUR_OF_DAY);
+    int myMinute = call.get(Calendar.MINUTE);
+    TextView tvTime, tvDate;
+    int DIALOG_DATE = 2;
+    int myYear = call.get(Calendar.YEAR);
+    int myMonth = call.get(Calendar.MONTH);
+    int myDay = call.get(Calendar.DAY_OF_MONTH);
+    int mCount;
+    boolean open = false;
+    String time = "time", date = "date";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        noteContentText = (EditText) findViewById(R.id.noteContentText);
+        noteNameText = (EditText) findViewById(R.id.noteNameText);
+        btnDone = (Button) findViewById(R.id.btnDone);
+        dbHelper = new DBHelper(this);
+
+        tvTime = (TextView) findViewById(R.id.tvTime);
+        tvDate = (TextView) findViewById(R.id.tvDate);
+        intent = new Intent(this, AlarmTask.class);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onclick (View view){
+        showDialog(DIALOG_TIME);
+    }
+    public void onclickDate(View view) {
+        showDialog(DIALOG_DATE);
+    }
+
+    protected Dialog onCreateDialog(int id){
+        if(id == DIALOG_TIME) {
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, true);
+            return tpd;
+        }
+        if (id == DIALOG_DATE) {
+            DatePickerDialog dpd = new DatePickerDialog(this, myCallBack2, myYear, myMonth, myDay);
+            return dpd;
+        }
+
+        return super.onCreateDialog(id);
+    }
+
+    TimePickerDialog.OnTimeSetListener myCallBack = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHour = hourOfDay;
+            myMinute = minute;
+
+            call.set(Calendar.HOUR_OF_DAY, myHour);
+            call.set(Calendar.MINUTE, myMinute);
+            call.set(Calendar.SECOND, 0);
+            call.set(Calendar.MILLISECOND, 0);
+
+            open = true;
+
+            TimeVisible();;
+        }
+    };
+    public void TimeVisible(){
+        time = (myHour + " : " + myMinute);
+        tvTime.setText(time);
+        date = (myYear + "/" + myMonth + "/" + myDay);
+        tvDate.setText(date);
+    }
+
+    DatePickerDialog.OnDateSetListener myCallBack2 = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myYear = year;
+            myMonth = monthOfYear;
+            myDay = dayOfMonth;
+            call.set(Calendar.YEAR, year);
+            call.set(Calendar.MONTH, monthOfYear);
+            call.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            tvDate.setText(myDay + "/" + myMonth + "/" + myYear);
+
+            open = true;
+            TimeVisible();
+        }
+    };
+    public void kre (){
+
+        AlarmTask am = new AlarmTask(this, call);
+        am.setText(noteContentText.getText().toString(), mCount, noteNameText.getText().toString());
+        am.run();
+    }
+
+
+    private String saveNote() {
+        Date date = new Date();
+        SimpleDateFormat namePattern = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+        String fileName = namePattern.format(date);
+        try {
+            OutputStream outputStream = openFileOutput(fileName + "." + FILE_EXTENSION, 0);
+            OutputStreamWriter osw = new OutputStreamWriter(outputStream);
+            osw.write(noteContentText.getText().toString());
+            osw.close();
+        } catch (Throwable t) {
+            Log.d("TAG", t.toString());
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+        return fileName;
+    }
+    final String LOG_TAG = "myLogs";
+
+    public void onclickDone(View view){
+        if (!noteContentText.getText().toString().isEmpty()) {
+            ContentValues cv = new ContentValues();
+            notesDB = dbHelper.getWritableDatabase();
+            String noteName = noteNameText.getText().toString();
+            String fileName = saveNote();
+            cv.put(DBHelper.FILE_NAME_COLUMN, fileName);
+            cv.put(DBHelper.NOTE_NAME_COLUMN, noteName);
+            cv.put(DBHelper.FILE_DATE_COLUMN, date);
+            cv.put(DBHelper.FILE_TIME_COLUMN, time);
+            // notesDB.insert(DBHelper.TABLE_NAME, null, cv);
+            mCount = (int) notesDB.insert(DBHelper.TABLE_NAME, null, cv);
+            Log.d(LOG_TAG, "row inserted, ID = " + mCount);
+            dbHelper.close();
+            notesDB.close();
+        }
+        if (open) {
+            kre();
+        }
+        super.onBackPressed();
+    }
+}
+
+
+
---
+++ to_do_list/app/src/main/java/com/developers/notes/MainActivity.java
@@ -0,0 +1,158 @@
+package com.developers.notes;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.SparseIntArray;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import static com.developers.notes.DBHelper.TABLE_NAME;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static SparseIntArray ids = new SparseIntArray();
+    FloatingActionButton fab;
+    ListView noteList;
+    NoteAdapter noteAdapter;
+    Cursor cursor;
+    DBHelper dbHelper;
+    SQLiteDatabase notesDB;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        noteList = (ListView) findViewById(R.id.noteList);
+        dbHelper = new DBHelper(this);
+        notesDB = dbHelper.getReadableDatabase();
+        cursor = notesDB.query(TABLE_NAME, null, null, null, null, null, null);
+        noteAdapter = new NoteAdapter(MainActivity.this, cursor, 0);
+        noteList.setAdapter(noteAdapter);
+        noteList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                cursor = notesDB.query(DBHelper.TABLE_NAME, null, null, null, null, null, null);
+                cursor.moveToPosition(position);
+                String fileName = cursor.getString(cursor.getColumnIndex(DBHelper.FILE_NAME_COLUMN));
+                String noteName = cursor.getString(cursor.getColumnIndex(DBHelper.NOTE_NAME_COLUMN));
+                Intent editNote = new Intent(MainActivity.this, EditNoteActivity.class);
+                editNote.putExtra("filename", fileName);
+                editNote.putExtra("notename", noteName);
+                int unicId = (int) id;
+                editNote.putExtra("kolvo", unicId);
+                startActivity(editNote);
+            }
+        });
+
+        View.OnClickListener fabOnClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createNote = new Intent(MainActivity.this, CreateNoteActivity.class);
+                startActivity(createNote);
+            }
+        };
+        fab.setOnClickListener(fabOnClick);
+
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.setDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
+        noteList.setAdapter(noteAdapter);
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        noteAdapter.notifyDataSetChanged();
+        noteList.setAdapter(noteAdapter);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.notifyDataSetChanged();
+        noteList.setAdapter(noteAdapter);
+    }
+
+    public void onBackPressed() {
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+            super.onBackPressed();
+//        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
